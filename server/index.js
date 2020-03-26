2da81dc31c6414b15d343a29130aa781c336979d
Nicholas Heo <nicholas.heo95@gmail.com>
2020-03-25 21:11:34 -0400

    first commit, showing three modules

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 4001;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/../public")));
+
+app.listen(port, () => {
+  `Proxy server is running on port ${port}`;
+});
