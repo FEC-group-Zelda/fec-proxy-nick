db5ec3127f74be1ab1394bd15b0e5ae6e699bc19
Nicholas Heo <nicholas.heo95@gmail.com>
2020-03-25 22:44:16 -0400

    rendering all 4 modules..

--- server/index.js
+++ server/index.js
@@ -6,5 +6,5 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "/../public")));
 
 app.listen(port, () => {
-  `Proxy server is running on port ${port}`;
+  console.log(`Proxy server is running on port ${port}`);
 });
