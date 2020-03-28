fcde0bbc9f1e6f2ce1511b84495384d992d1302d
Nicholas Heo <nicholas.heo95@gmail.com>
2020-03-28 13:07:14 -0400

    proxy for local

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const port = 4001;
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "/../public")));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.listen(port, () => {
   console.log(`Proxy server is running on port ${port}`);
 });
